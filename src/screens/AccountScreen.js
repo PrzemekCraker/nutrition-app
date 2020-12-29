7aef65442324ed4067a49c0ca0f9cb202e5b1b22
PrzemekCraker <przemekzbigniew@gmail.com>
2020-12-29 21:16:58 +0100

    account-boiler

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: "Account",
+  tabBarIcon: <FontAwesome name="gear" size={20} />,
 };
 
 const styles = StyleSheet.create({});
